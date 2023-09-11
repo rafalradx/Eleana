402248304c78a17ebf1b65b9d20e04f754bb8757
Marcin <marcin.sarewicz@gmail.com>
2023-09-11 15:08:55 +0200

    Naprawiona klasa co ją Rafał popsuł

--- subprogs/general_eleana_methods.py
+++ subprogs/general_eleana_methods.py
@@ -32,7 +32,11 @@ class Eleana():
             'type': '',
         }
 
-        comments = notes 
+        comments = {"content": "",
+             "tags": {"bold": [], "italic": [], "code": [], "normal size": [], "larger size": [], "largest size": [],
+                      "highlight": [], "highlight red": [], "highlight green": [], "highlight black": [],
+                      "text white": [],
+                      "text grey": [], "text blue": [], "text green": [], "text red": []}}
 
         data_x = np.array([])
         data_y = np.array([])
