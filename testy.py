d77d2577e825346ff6dfeb94275a5c9bd0db1202
KarritPrimorph <marcin.sarewicz@gmail.com>
2023-09-11 23:50:09 +0200

    Zmiana metody czytania plkiów DSC. Trzeba cały kod przerobić na czytanie pojedynczego a nie całości na raz

--- testy.py
+++ testy.py
@@ -1,7 +1,14 @@
-z = "abcde"
+x_axis = []
+i = 0
+x_min = 150
+x_wid = 3500
+res = 4096
 
-rev = z[:-2]
+step = x_wid/res
+print(step)
 
-print(rev)
+for i in range(0,res):
+    x_axis.append(i*step+x_min)
 
-print("Mateusz tu byl")
+print(x_axis)
+print(len(x_axis))
