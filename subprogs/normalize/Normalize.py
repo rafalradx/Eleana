789fac7725b51b472a798827ea0f7a8d8b0c4404
KarritPrimorph <marcin.sarewicz@gmail.com>
2024-09-04 17:21:20 +0200

    Work on Normalize 5

--- subprogs/normalize/Normalize.py
+++ subprogs/normalize/Normalize.py
@@ -4,32 +4,50 @@ import tkinter as tk
 import pygubu
 import copy
 import numpy as np
-from normalize.Normalizeui import NormalizeUI
-from CTkSpinbox import CTkSpinbox
-from Observer import Observer
+
+''' For testing comment following imports 
+    For use in application uncomment the following'''
+# from normalize.Normalizeui import NormalizeUI
+# from CTkSpinbox import CTkSpinbox
+# from Observer import Observer
+
+''' For testing uncomment the following imports 
+    For use in application comment the following'''
+from Normalizeui import NormalizeUI
 
 class Normalize(NormalizeUI):
     ''' --- IN YOUR NEW COPY AND PASTE FROM HERE --- '''
-    def __init__(self, application, which = 'first', batch_mode = False):
-            # Create references
-        self.app = application
-        self.master = self.app.mainwindow
-        self.eleana = self.app.eleana
-        self.grapher = self.app.grapher
+    def __init__(self, app  = None, which = 'first', batch_mode = False):
+        if app:
+            self.app = app
+            self.master = self.app.mainwindow
+            self.eleana = self.app.eleana
+            self.grapher = self.app.grapher
+        else:
+            self.app = None
+            self.master = None
+            self.eleana = None
+            self.grapher = None
+
+        # Set to which selection 'First' or 'Second'
         self.which = which
-            # Create batch mode state
+
+        # Create batch mode state
         self.batch = batch_mode
-        # Do not build window if batch mode is true
-        if not self.batch:
-            super().__init__(self.master)
+        # Do not build window if batch mode is true or app is not defined
+        if self.batch == False and self.app:
+            # Create window
+            if self.app:
+                super().__init__(self.master)
+                # Configure the window
+                self.configure_window()
             # Create observer
-        self.observer = Observer(self.eleana, self)
+            self.observer = Observer(self.eleana, self)
+            self.eleana.notify_on = True
             # Initialize data to modify
-        self.get_data(start=True)
+            self.get_data(start=True)
             # Set current position in Results Dataset
-        self.result_index = len(self.eleana.results_dataset)
-        self.eleana.notify_on = True
-        self.configure_window()
+            self.result_index = len(self.eleana.results_dataset)
 
     def get(self):
         ''' Returns self.response to the main application after close '''
@@ -113,20 +131,48 @@ class Normalize(NormalizeUI):
                 --- TO HERE --- 
     """""""""""""""""""""""""""""""""""""""""""""""""'''
 
-    def perform_calculation(self):
+    def perform_calculation(self, normalize_to = None, y_data = None, x_data = None):
         ''' Method that calculates something in your subprogram
             This must be prepared for a single data
             But you must check if it works for
             all possible data types
         '''
-        normalize_to = float(self.spinbox.get())
-        min = np.min(self.original_data.y)
-        max = np.max(self.original_data.y)
-        glob_min = np.min(min)
-        glob_max = np.max(max)
-        print(glob_min)
-        print(glob_max)
+        if not normalize_to:
+            normalize_to = float(self.spinbox.get())
+            y_data = self.original_data.y
+            x_data = self.original_data.x
+        ''' PUT YOUR CODE BELOW '''
+        list_of_max = []
+        if y_data:
+            for data in y_data:
+                if data:
+                    min = np.min(data)
+                    max = np.max(data)
+                    delta = float(max - min)
+                    list_of_max.append(delta)
+                    amplitude = sorted(list_of_max, reverse=True)[0]
+                else:
+                    amplitude = None
+                    continue
+        else:
+            amplitude = None
+        print(amplitude)
+        # min = np.min(y_data)
+        # max = np.max(y_data)
+        #
+        # glob_min = np.min(min)
+        # glob_max = np.max(max)
+        # print(glob_min)
+        # print(glob_max)
 
 if __name__ == "__main__":
-    app = Normalize()
-    app.run()
+    subprogram = Normalize()
+    # Test calculations
+    y_data = np.array([])
+    # y_data = np.array([
+    #                       [1,2,3],
+    #                       [7.1,61.2,81.6],
+    #                       [1,2,3]
+    #                       ])
+    subprogram.perform_calculation(normalize_to = 5, y_data = y_data)
+
