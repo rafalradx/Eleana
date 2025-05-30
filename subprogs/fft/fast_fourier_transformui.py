b5892ee2f80bfb649dea1ceda70a92505f0dc879
Rafał Pietras <rafal.radx@gmail.com>
2025-05-30 12:33:44 +0200

    add FFT menu entry and window

---
+++ subprogs/fft/fast_fourier_transformui.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+import pathlib
+import tkinter as tk
+import pygubu
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "fast_fourier_transform.ui"
+RESOURCE_PATHS = [PROJECT_PATH]
+
+
+class FastFourierUI:
+    def __init__(
+        self,
+        master=None,
+        translator=None,
+        on_first_object_cb=None,
+        data_pool=None
+    ):
+        self.builder = pygubu.Builder(
+            translator=translator,
+            on_first_object=on_first_object_cb,
+            data_pool=data_pool
+        )
+        self.builder.add_resource_paths(RESOURCE_PATHS)
+        self.builder.add_from_file(PROJECT_UI)
+        # Main widget
+        self.mainwindow: tk.Toplevel = self.builder.get_object(
+            "toplevel1", master)
+        self.builder.connect_callbacks(self)
+
+    def center_window(self):
+        if self.mainwindow.winfo_ismapped():
+            min_w, min_h = self.mainwindow.wm_minsize()
+            max_w, max_h = self.mainwindow.wm_maxsize()
+            screen_w = self.mainwindow.winfo_screenwidth()
+            screen_h = self.mainwindow.winfo_screenheight()
+            final_w = min(
+                screen_w,
+                max_w,
+                max(
+                    min_w,
+                    self.mainwindow.winfo_width(),
+                    self.mainwindow.winfo_reqwidth(),
+                ),
+            )
+            final_h = min(
+                screen_h,
+                max_h,
+                max(
+                    min_h,
+                    self.mainwindow.winfo_height(),
+                    self.mainwindow.winfo_reqheight(),
+                ),
+            )
+            x = (screen_w // 2) - (final_w // 2)
+            y = (screen_h // 2) - (final_h // 2)
+            geometry = f"{final_w}x{final_h}+{x}+{y}"
+
+            def set_geometry():
+                self.mainwindow.geometry(geometry)
+
+            self.mainwindow.after_idle(set_geometry)
+        else:
+            # Window is not mapped, wait and try again later.
+            self.mainwindow.after(5, self.center_window)
+
+    def run(self, center=False):
+        if center:
+            self.center_window()
+        self.mainwindow.mainloop()
+
+    def windowing_selected(self, value):
+        pass
+
+    def ok_clicked(self):
+        pass
+
+    def show_report_clicked(self):
+        pass
+
+    def process_group_clicked(self):
+        pass
+
+    def clear_report_clicked(self):
+        pass
+
+    def cancel(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = FastFourierUI()
+    app.run()
